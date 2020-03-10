ff3ad4c813f5ea5e4134934c3585abb4dd54ae1e
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-10 23:30:36 +0100

    ajoute de le methode calculSalaire

--- exo32/src/main/java/Uvsq21923164/exo32/Employe.java
+++ exo32/src/main/java/Uvsq21923164/exo32/Employe.java
@@ -2,4 +2,16 @@ package Uvsq21923164.exo32;
 
 public class Employe {
 
+	private int nbAnnees;
+	private Coordonnees coordonnees;
+
+	public String getCoordonnees() {
+		return coordonnees.toString();
+	}
+	public double calculSalaire() {
+		return 1500 + nbAnnees * 20;
+	}
+	
+	
+	
 }
