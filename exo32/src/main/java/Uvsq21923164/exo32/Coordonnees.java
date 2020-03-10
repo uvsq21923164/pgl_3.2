2dacd7d5d244d5e6b70bc4c2997bfbd5ad221c02
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-10 23:35:07 +0100

    creation de la classe coordonnees

---
+++ exo32/src/main/java/Uvsq21923164/exo32/Coordonnees.java
@@ -0,0 +1,11 @@
+package Uvsq21923164.exo32;
+
+public class Coordonnees {
+	private String nom;
+	private String adresse;
+	
+	@Override
+	public String toString() {
+		return nom + " - " + adresse;
+	}
+}
--- exo32/src/main/java/Uvsq21923164/exo32/Vendeur.java
+++ exo32/src/main/java/Uvsq21923164/exo32/Vendeur.java
@@ -1,5 +1,11 @@
 package Uvsq21923164.exo32;
 
-public class Vendeur {
-
+public class Vendeur extends Employe {
+private double commission;
+	
+	@Override
+	public double calculSalaire() {
+		return super.calculSalaire() + commission;
+	}
+	
 }
