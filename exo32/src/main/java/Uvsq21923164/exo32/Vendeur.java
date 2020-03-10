07f40a90b7c3172057bdab0b8149bc594789dc51
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-11 00:02:03 +0100

    ajoute de la methode Calculsalaire

--- exo32/src/main/java/Uvsq21923164/exo32/Vendeur.java
+++ exo32/src/main/java/Uvsq21923164/exo32/Vendeur.java
@@ -1,5 +1,16 @@
 package Uvsq21923164.exo32;
 
-public class Vendeur {
+public class Vendeur extends Employe {
+	private double commission;
+public Vendeur(String nom, int annee) {
+		super(nom, annee);
+		// TODO Auto-generated constructor stub
+	}
 
+
+	
+	@Override
+	public double calculSalaire() {
+		return super.calculSalaire() + commission;
+	}
 }
