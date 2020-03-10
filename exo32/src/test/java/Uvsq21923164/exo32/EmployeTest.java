dbf12917f267c0c511bca53e8d0fe7bec9421a53
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-11 00:06:53 +0100

    creation de la classe Employetest

--- exo32/src/test/java/Uvsq21923164/exo32/EmployeTest.java
+++ exo32/src/test/java/Uvsq21923164/exo32/EmployeTest.java
@@ -1,7 +1,14 @@
 package Uvsq21923164.exo32;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class EmployeTest extends TestCase {
-
+	@Test
+	public void CalculSalaireEmployeTest() {
+		Employe E= new Employe("John SMITH",2015);
+		double expected=1600;
+		assert (E.calculSalaire()==expected);
+	}
 }
