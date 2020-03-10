38c5695ad8faffa9e053901e04e1e7b42ca98311
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-11 00:10:21 +0100

    modifer la classe employetest

--- exo32/src/test/java/Uvsq21923164/exo32/EmployeTest.java
+++ exo32/src/test/java/Uvsq21923164/exo32/EmployeTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestCase;
 public class EmployeTest extends TestCase {
 	@Test
 	public void CalculSalaireEmployeTest() {
-		Employe E= new Employe("John SMITH",2015);
+		Employe E= new Employe("youcef",2018);
 		double expected=1600;
 		assert (E.calculSalaire()==expected);
 	}
