2696b7808bef1b14fce9ec6e776f40c79a66f2f0
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-19 14:26:31 +0100

    ajoute la classe testEmploye

--- exo32/src/test/java/Uvsq21923164/exo32/EmployeTest.java
+++ exo32/src/test/java/Uvsq21923164/exo32/EmployeTest.java
@@ -2,13 +2,13 @@ package Uvsq21923164.exo32;
 
 import org.junit.Test;
 
-import junit.framework.TestCase;
 
-public class EmployeTest extends TestCase {
+
+public class EmployeTest  {
 	@Test
 	public void CalculSalaireEmployeTest() {
-		Employe E= new Employe("youcef",2018);
-		double expected=1600;
+		Employe E= new Employe("youcef",2020);
+		double expected=1500;
 		assert (E.calculSalaire()==expected);
 	}
 }
