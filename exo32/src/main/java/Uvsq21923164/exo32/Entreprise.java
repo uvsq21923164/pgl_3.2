6fda9d8d44f0a78d7cbced1e23fd41f55fc89310
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-19 14:20:50 +0100

    Ajoute la methode calculSalaire et addemploye

--- exo32/src/main/java/Uvsq21923164/exo32/Entreprise.java
+++ exo32/src/main/java/Uvsq21923164/exo32/Entreprise.java
@@ -5,15 +5,18 @@ import java.util.List;
 
 public class Entreprise {
 
-	private List<Salarie> salarieList;
+	private List<Employe> salarieList;
 	
 	public Entreprise() {
-		salarieList = new ArrayList<Salarie>();
+		salarieList = new ArrayList<Employe>();
+	}
+	public void addEmploye(Employe E) {
+		this.salarieList.add(E);
 	}
 	
 	public double getSommeTotaleSalaires() {
 		double sum = 0;
-		for (Salarie s : salarieList) {
+		for (Employe s : salarieList) {
 			sum += s.calculSalaire();
 		}
 		return sum;
